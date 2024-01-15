69d1abe28bc8f1562d7dc6d4d50ed37bf68ed6e6
AbdulKadir Demirkaya <126807887+kadirdemirkaya@users.noreply.github.com>
2024-01-15 23:21:13 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,11 @@
+# GrpcApp
+
+Bu projede Client ve Server olmak üzere iki proje arasında http2 protokolü ile hem projede hem de dağıtımda ki iletişiminden bahsetmektedir.Aynı sırada bir yönlendirici ile istekleri yönlendirmektedir.
+
+## Gerekli Teknoloji veya Yazılımlar
+
+- .Net Core
+- Ocelot
+- Docker
+- Swagger or Postman
+- k8s
--- src/gRPCServerApp/obj/Debug/net7.0/gRPCServerApp.AssemblyInfo.cs
+++ src/gRPCServerApp/obj/Debug/net7.0/gRPCServerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gRPCServerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+822c69a45d015bf1b57ebd08c66623a96c57b4d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("gRPCServerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gRPCServerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
